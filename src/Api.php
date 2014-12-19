a4eed5d0afb989be32104b67a7fbeabe3470099a
Jevon Wright <jevon@jevon.org>
2014-12-19 13:10:41 +1300

    Output the correct JSON content type on APIs

--- src/Api.php
+++ src/Api.php
@@ -9,5 +9,22 @@ abstract class Api {
   abstract function getEndpoint();
 
   // TODO caching
+  function render($arguments) {
+    header("Content-Type: application/json");
+
+    try {
+      $json = array('success' => true, 'result' => $this->getJSON($arguments));
+      echo json_encode($json);
+
+    } catch (\Exception $e) {
+      // render an API exception
+      $json = array('success' => false, 'error' => $e->getMessage());
+      echo json_encode($json);
+
+      if (function_exists('log_uncaught_exception')) {
+        log_uncaught_exception(new CaughtApiException("API threw '" . $e->getMessage() . "'", $e));
+      }
+    }
+  }
 
 }
